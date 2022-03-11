80145d67639def6ff872d98b1aefe720ab32dc38
m.saccoccini1997@gmail.com <m.saccoccini1997@gmail.com>
2022-03-11 07:41:53 +0100

    header component done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SpotifyHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SpotifyHeader from './components/SpotifyHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SpotifyHeader
   }
 }
 </script>
@@ -22,7 +21,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/SpotifyHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="wrapper">
+        <div class="img">
+            <img src="../assets/spotify_logo.png" alt="">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SpotifyHeader'
+    }
+</script>
+
+
+<style lang="scss" scoped>
+$grey: #575757;
+
+.wrapper{
+width: 100vw;
+height: 100px;
+background-color: $grey;
+margin: 0 auto;
+display: flex;
+align-items: center;
+    .img{
+        width: 50px;
+        margin-left: 20px;
+        img{
+            width: 70px;
+        } 
+    }
+}
+</style>
